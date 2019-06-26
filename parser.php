c281a21c03b84eba6e38bc7338dd05cb153b5f34
davidzyuz <zyuzdavid@gmail.com>
2019-06-26 19:11:16 +0300

    the parser is ready to use

--- parser.php
+++ parser.php
@@ -4,36 +4,39 @@ ini_set('display_errors', 1);
 
 define ('zp', 'http://www.gismeteo.ua/city/daily/5093/');
 
-//$remote_content = file_get_contents(zp);
+function bwt_parser() {
+
+	$remote_content = file_get_contents(zp);
+
+	$store = fopen('./data.txt', "r+");
+	fwrite($store, $remote_content);
+	fclose($store);
+
+	unset($remote_content);
+
+	$local_content = file('./data.txt', 2);
+
+	$html_dump = '';
+
+	foreach($local_content as $key => $value) {
+
+		$html_dump .= $value;
+	}
+
+	$clean_text = strip_tags($html_dump);
 
 
-// $store = fopen('./data.txt', "r+");
-// fwrite($store, $remote_content);
-// fclose($store);
+	// return a string with all info we need 
+	$start_str = mb_strpos($clean_text, 'Погода в Запорожье');
+	$end_str = mb_strrpos($clean_text, 'По ощущению'); 
+	$length = $end_str - $start_str;
+	$search_area = mb_substr($clean_text, $start_str, $length);
+	$search_area = preg_replace('/\s+/', ' ', $search_area);
+	$useful_info = explode(' ', $search_area);
 
-//unset($remote_content);
-
-$local_content = file('./data.txt', 2);
-
-$dirty_output = '';
-
-foreach($local_content as $key => $value) {
-	// $output .= $key . '<br />';
-	$dirty_output .= $value;
+	return $useful_info; // a keys we need has index 2 and 6;
 }
 
-$clean_output = strip_tags($dirty_output);
-// return a string with all info we need 
-$start_str = strpos($clean_output, 'Погода в Запорожье');
-$end_str = strrpos($clean_output, 'Завтра'); 
-$length = $end_str - $start_str;
-$search_area = substr($clean_output, $start_str, $length);
-echo $search_area;
-
-// print_r(htmlspecialchars($output));
-/* $pattern = '/([а-я]+)/';//|(\+\d+\.\d+|\+\d+)
-
-mb_ereg($pattern, $output, $m);
 echo '<pre>';
-print_r($m);
-echo '</pre>'; */
+print_r(bwt_parser());
+echo '</pre>';
